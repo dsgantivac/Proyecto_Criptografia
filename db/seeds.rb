48b624e94ce0e252496260044f0eb13970bd9fd7
asantamariap <asantamariap@unal.edu.co>
2018-09-30 18:49:48 +0000

    Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+require "./app/controllers/Algorithms/des.rb"
+require "./app/controllers/Algorithms/polarcrypt.rb"
+
+puts 'Llenando'
+20.times do |row|
+  
+  user=User.create!(email: row.to_s+"@"+row.to_s+".com",
+  password: row, 
+  conf_password: row, 
+  name: Faker::Name.female_first_name,
+  pin: (0...8).map { (65 + rand(26)).chr }.join)
+
+  rand(5..20).times do
+    cr=""
+    text=""
+    enc=rand(1..2)
+    
+    ran=rand(1..10)
+    
+    if ran <=3
+      text=Faker::HarryPotter.quote.sub("’", "'").to_s.force_encoding("ASCII-8BIT")
+    elsif ran <=6
+      text=Faker::ChuckNorris.fact.sub("’", "'").to_s.force_encoding("ASCII-8BIT")
+    else
+      text=Faker::ChuckNorris.fact.sub("’", "'").to_s.force_encoding("ASCII-8BIT")
+    end 
+    if enc == 1
+      cr="DES"
+      text=encrypt(text, user.pin)
+    else
+      cr="PolarCrypt"
+      text=encriptar(text, user.pin)
+    end
+    Article.create!(
+      encryptType: cr,
+      user_id: user.id,
+      title: Faker::Date.between(2.months.ago, Date.today), 
+      body: text 
+    )
+  
+  end
+
+end
